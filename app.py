776611f04cbfad2de573df75434d9ac4c251d11c
Ibrahim18-95 <ibrahimsamejo421@gmail.com>
2025-08-27 16:04:37 +0500

    Add Logout Function

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask , redirect , request , render_template 
+from flask import Flask, redirect, request, render_template, url_for
 
 app = Flask(__name__)
 
@@ -6,10 +6,19 @@ app = Flask(__name__)
 def Login():
     return render_template("Login.html")
 
-@app.route("/submit", methods = ["post"])
+@app.route("/submit", methods=["POST"])
 def Submit():
     Username = request.form.get("username")
     Password = request.form.get("password")
     
     if Username == "admin" and Password == "12345":
-        return render_template("Welcome.html", name = Username)
+        return render_template("Welcome.html", name=Username)
+    else:
+        return render_template("Login.html", error="Invalid username or password")
+
+@app.route("/logout")
+def logout():
+    return redirect(url_for("login"))
+
+if __name__ == "__main__":
+    app.run(debug=True)
